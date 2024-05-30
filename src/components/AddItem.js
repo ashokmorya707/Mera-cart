6530c064a66bb4cbd07e6089b43be69b6f30d7b7
ashokmorya707 <ashokmorya707@gmail.com>
2024-05-31 03:32:04 +0530

    first commit

---
+++ src/components/AddItem.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+class AddItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      productName: "",
+      productPrice: 0,
+    };
+  }
+  render() {
+    return (
+      <form
+        className="row mb-5"
+        onSubmit={(e) => {
+          e.preventDefault();
+          this.props.addItem(
+            this.state.productName,
+            Number(this.state.productPrice)
+          );
+        }}
+      >
+        <div className="form-group col-4">
+          <label htmlFor="inputName">Name</label>
+          <input
+            type="text"
+            className="form-control"
+            id="inputName"
+            aria-describedby="name"
+            name="productName"
+            onChange={(e) => {
+              this.setState({ productName: e.currentTarget.value });
+            }}
+            value={this.state.productName}
+          />
+        </div>
+        <div className="form-group col-4">
+          <label htmlFor="inputPrice">Price</label>
+          <input
+            type="number"
+            className="form-control"
+            id="price"
+            name="productPrice"
+            onChange={(e) => {
+              this.setState({ productPrice: Number(e.currentTarget.value) });
+            }}
+            value={this.state.productPrice}
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary col-4">
+          Add
+        </button>
+      </form>
+    );
+  }
+}
+
+export default AddItem;
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+class Navbar extends Component {
+  // state = {};
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          Mera-Cart
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
